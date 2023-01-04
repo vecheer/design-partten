88778249e69701b93cc9ce100788ca3aefe62738
vecheer <1571462409@.com>
2023-01-04 11:37:30 +0800

    优化

--- CH1-OOP/src/main/java/L3_Id_Generator/Generator.java
+++ CH1-OOP/src/main/java/L3_Id_Generator/Generator.java
@@ -1,12 +1,9 @@
 package L3_Id_Generator;
 
 
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-import java.time.temporal.TemporalAccessor;
-import java.util.UUID;
 
 /**
  * ID 生成器
@@ -19,15 +16,37 @@ public class Generator {
 
     private static final String SEPARATOR = "-";
     private static final String DATA_FORMAT = "yyyyMMddHHmmssSSS";
+    private static final int RANDOM_LENGTH = 15;
+
+    private static final char[] CHAR_TABLE = {
+            'a','b','c','d','e','f','g','h','i','j',
+            'k','l','m','n','o','p','q','r','s','t',
+            'u','v','w','x','y','z',
+
+            '0','1','2','3','4','5','6','7','8','9',
+
+            'A','B','C','D','E','F','G','H','I','J','K',
+            'L','M','N','O','P','Q','R','S','T','U',
+            'V','W','X','Y','Z'
+    };
 
     public static String getId(CommonRequest request){
 
         LocalDateTime ldt = LocalDateTime.ofInstant(request.getDate().toInstant(), ZoneId.systemDefault());
         String time = DateTimeFormatter.ofPattern(DATA_FORMAT).format(ldt);
         String host = request.getHost().replace(".","");
-        String uuid = UUID.randomUUID().toString();
+        String randomId = randomStr(RANDOM_LENGTH);
 
-        return time + SEPARATOR + host + SEPARATOR + uuid;
+        return time + SEPARATOR + host + SEPARATOR + randomId;
     }
 
+
+    private static String randomStr(int length){
+        StringBuilder result = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            int random = (int) (Math.random() * CHAR_TABLE.length);
+            result.append(CHAR_TABLE[random]);
+        }
+        return result.toString();
+    }
 }
