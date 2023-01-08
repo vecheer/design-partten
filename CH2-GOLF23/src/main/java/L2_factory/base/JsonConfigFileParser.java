304a43e7e14313d271230345cef9aa84760ead55
vecheer <1571462409@.com>
2023-01-09 00:12:16 +0800

    工厂模式

---
+++ CH2-GOLF23/src/main/java/L2_factory/base/JsonConfigFileParser.java
@@ -0,0 +1,13 @@
+package L2_factory.base;
+
+/**
+ * @author yq
+ * @version 1.0
+ * @date 2023/1/8 23:45
+ */
+public class JsonConfigFileParser implements ConfigFileParser{
+    @Override
+    public Config parse(String filePath) {
+        return null;
+    }
+}
